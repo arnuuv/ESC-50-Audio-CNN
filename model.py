31d484a4d83a46a6b04bce8c060a0a1bf31ab8b5
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-09 11:08:29 +0300

    loop architecture

--- model.py
+++ model.py
@@ -29,3 +29,18 @@ class ResidualBlock(nn.Module):
         out_add = out+shortcut
         out = torch.relu(out.add)
         return out_add
+
+
+class AudioCNN(nn.Module):
+    def __init__(self, num_classes=50):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=4), nn.BatchNorm2d(64), nn.ReLU(inplace=True), nn.MaxPool2d(3, stride=2, padding=1))
+
+        self.layer1 = nn.ModuleList([ResidualBlock(64, 64) for i in range(3)])
+        self.layer2 = nn.ModuleList(
+            [ResidualBlock(64 if i == 0 else 128, 128) for i in range(4)])
+        self.layer3 = nn.ModuleList(
+            [ResidualBlock(64 if i == 0 else 256, 256) for i in range(6)])
+        self.layer4 = nn.ModuleList(
+            [ResidualBlock(64 if i == 0 else 512, 512) for i in range(3)])
