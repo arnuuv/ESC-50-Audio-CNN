be0c62587f030e6408285aeedb5585aa4ef40697
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-09 11:33:53 +0300

    esc data into dockerised container

--- train.py
+++ train.py
@@ -4,8 +4,20 @@ app = modal.App("audio-cnn")
 
 image = (modal.Image.debain_slim()
          .pip_install_from_requirements("requirements.txt")
-         .apt_install(["wget", "unzip"])
-         )
+         .apt_install(["wget", "unzip", "ffmpeg", "libsndfile1"])
+         .run_commands([
+             "cd /tmp && wget https://github.com/karolpiczak/ESC-50/archive/master.zip -0 esc50.zip"
+             "cd /tmp && unzip esc50.zip",
+             "mkdir -p /opt/esc50-data"
+             "cp -r /tmp/ESC-50-master/* /opt/esc50-data"
+             "rm -rf /tmp/esc50.zip /tmp/ESC-50-master"
+
+         ])
+
+         .add_local_python_source("model"))
+
+volume = modal.Volume.from_name("esc50-data", create_if_missing=True)
+modal_volume = modal.Volume.from_name("esc-model", create_if_missing=True)
 
 
 @app.function()
