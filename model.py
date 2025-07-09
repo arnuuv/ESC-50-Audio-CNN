d7de2ee71372aff346700414ac888a1cc4ddef95
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-09 11:12:18 +0300

    forward pass for 3 layers

--- model.py
+++ model.py
@@ -44,3 +44,23 @@ class AudioCNN(nn.Module):
             [ResidualBlock(64 if i == 0 else 256, 256) for i in range(6)])
         self.layer4 = nn.ModuleList(
             [ResidualBlock(64 if i == 0 else 512, 512) for i in range(3)])
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        for block in self.layer1:
+            x = block(x)
+        for block in self.layer2:
+            x = block(x)
+        for block in self.layer3:
+            x = block(x)
+        for block in self.layer4:
+            x = block(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x
