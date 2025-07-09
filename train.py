a53b7207b0b73d8cbc7b88b0f7e44c8261810d2c
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-09 14:37:34 +0300

    setting mel spectogram values

--- train.py
+++ train.py
@@ -3,8 +3,11 @@ import modal
 from torch.utils.data import Dataset
 import pandas as pd
 from pathlib import Path
-
+import torchaudio
+import torch
 import sys
+import torch.nn as nn
+import torchaudio.transforms as T
 
 app = modal.App("audio-cnn")
 
@@ -39,8 +42,29 @@ class ESC50Dataset(Dataset):
             self.metadata = self.metadata[self.metadata['fold'] == 5]
             
         self.classes = sorted(self.metadata['category'].unique())
-        
-    
+        self.class_to_idx = {cls: i for i, cls in enumerate(self.classes)}
+        self.metadata['label'] = self.metadata['category'].map(
+            self.class_to_idx
+        )
+        def __len__(self):
+            return len(self.metadata)
+         
+        def __getitem__(self,idx):
+            row = self.metadata.iloc[idx]
+            audio_path = self.data_dir / "audio"/ row['filename']
+            
+            waveform,sample_rate = torchaudio.load(audio_path)
+            
+            if waveform.shape[0]>1: #[channel,samples] [2,44000] [1,44000]
+                waveform = torch.mean(waveform,dim=0,keepdim=True)
+            if self.transform:
+                spectogram = self.transform(waveform)
+            else:
+                spectogram = waveform
+            return spectogram, row['label']
+                
+                
+                
 @app.function(
     image=image,
     gpu="A10G",
@@ -48,7 +72,47 @@ class ESC50Dataset(Dataset):
     timeout=60 * 60 * 3
 )
 def train():
-    print("training")
+    esc50_dir = Path("/opt/esc-50-data")
+    train_transform = nn.Sequential(
+        T.MelSpectogram(
+            sample_rate = 22050,
+            n_fft = 1024,
+            hop_length = 512,
+            n_mels = 128,
+            f_min =0,
+            f_max = 11025
+    ),
+        T.AmplitudeToDB(),
+        T.FrequencyMasking(freq_mask_param = 30),
+        T.TimeMasking(time_mask_param = 80)
+        
+        
+        )
+    
+    val_transform = nn.Sequential(
+        T.MelSpectogram(
+            sample_rate = 22050,
+            n_fft = 1024,
+            hop_length = 512,
+            n_mels = 128,
+            f_min =0,
+            f_max = 11025
+    ),
+        T.AmplitudeToDB()    
+        )
+
+    train_dataset = ESC50Dataset(
+        data_dir = esc50_dir,
+        metadata_file = esc50_dir / "meta" / "esc50.csv",
+        split = 'train',
+        transform = train_transform
+    )
+    
+    val_dataset = ESC50Dataset(
+        data_dir = esc50_dir,
+        metadata_file = esc50_dir / "meta" / "esc50.csv",
+        split = 'val',
+        transform = val_transform)
 
 
 @app.local_entrypoint()
